010e4f6d7e81adee93bda70a4d4639f34d9aaa5d
wangkai <544094478@qq.com>
2017-09-14 20:21:28 +0800

    Fix the chapt9 code

--- chapt9/init/entry.c
+++ chapt9/init/entry.c
@@ -23,5 +23,21 @@ int kern_entry()
 	printk("kernel in memory end:   0x%08X\n",kern_end);	
 	printk("kernel in memory used: %d kb\N",(kern_end - kern_start + 1023)/1024);
 	show_memory_map();	
+	init_memory_map();
+
+	printk_color(rc_black,rc_red,"\nThe count of physical memory page is:%u\n\n",phy_page_count);
+	uint32_t alloc_addr = NULL;
+	
+	printk_color(rc_black,rc_light_brown,"Test physical memory alloc:\n")
+	
+	alloc_addr = pmm_alloc_page();
+	printk_color(rc_black,rc_light_brown,"Alloc physical addr:0x%08X\n",alloc_addr);
+
+	alloc_addr = pmm_alloc_page();
+	printk_color(rc_black,rc_light_brown,"Alloc physical addr:0x%08X\n",alloc_addr);
+	alloc_addr = pmm_alloc_page();
+	printk_color(rc_black,rc_light_brown,"Alloc physical addr:0x%08X\n",alloc_addr);
+	alloc_addr = pmm_alloc_page();
+	printk_color(rc_black,rc_light_brown,"Alloc physical addr:0x%08X\n",alloc_addr);
 	return 0;
 }
